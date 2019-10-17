74a42117c0920f13516029d4bd52adbafb35dff2
raulbs7 <48548673+raulbs7@users.noreply.github.com>
2019-10-17 02:03:45 +0200

    Update frontier.py

--- frontier.py
+++ frontier.py
@@ -10,20 +10,19 @@ class Frontier:
         else:
             count = len(self.frontier) - 1
             while count >= 0:
-                if self.frontier[count].f < treeNode.f:
+                if self.frontier[count].f <= treeNode.f:
                     self.frontier.insert(count+1, treeNode)
                     break
-                elif self.frontier[count].f == treeNode.f:
-                    if self.frontier[count].id < treeNode.id:
-                        self.frontier.insert(count+1, treeNode)
-                        break
                 count -= 1
             if count < 0:
                 self.frontier.insert(0, treeNode)
                  
     def remove(self):
-        treeNode = self.frontier.pop(0)
-        return treeNode
+        if self.frontier:
+            treeNode = self.frontier.pop(0)
+            return treeNode
+        else:
+            return none
 
     def isEmpty(self):
         if not self.frontier:
