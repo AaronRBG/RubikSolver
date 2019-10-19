536a1877c415dfa5ec5ac4492c4526a4089d8e67
ERV501 <44229730+ERV501@users.noreply.github.com>
2019-10-19 20:24:25 +0200

    Create frontier.py

---
+++ Final/frontier.py
@@ -0,0 +1,30 @@
+class Frontier:
+
+    def __init__(self):
+        self.frontier = []
+
+    def insert(self, treeNode):
+        if len(self.frontier) == 0:
+            self.frontier.append(treeNode)
+        else:
+            count = len(self.frontier) - 1
+            while count >= 0:
+                if self.frontier[count].f <= treeNode.f:
+                    self.frontier.insert(count + 1, treeNode)
+                    break
+                count -= 1
+            if count < 0:
+                self.frontier.insert(0, treeNode)
+
+    def remove(self): #Remove node from the frontier
+        if self.frontier:
+            treeNode = self.frontier.pop(0)
+            return treeNode
+        else:
+            return None
+
+    def isEmpty(self):
+        if not self.frontier:
+            return True
+        else:
+            return False
