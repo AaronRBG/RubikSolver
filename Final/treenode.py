9ff8082a75f07d2ea0900b9d8d5d288d9d00a451
raulbs7 <48548673+raulbs7@users.noreply.github.com>
2019-10-26 21:19:31 +0200

    Update treenode.py

--- Final/treenode.py
+++ Final/treenode.py
@@ -1,6 +1,6 @@
 class Treenode:
 	
-    def __init__(self, id, state, cost, action, d, f, parent=None):
+    def __init__(self, state, cost, action, d, f, parent=None):
         self.state = state
         self.parent = parent
         self.cost = cost
