941278a7f9cf162a7ac5dfd2e2c91236d026944b
Aharon1377 <48285149+Aharon1377@users.noreply.github.com>
2019-11-05 23:16:34 +0100

    Delete treenode.py

--- treenode.py
+++
@@ -1,9 +0,0 @@
-class Treenode:
-	
-    def __init__(self, id, state, cost, action, d, f, parent=None):
-        self.state = state
-        self.parent = parent
-        self.cost = cost
-        self.action = action
-        self.d = d
-        self.f = f       
