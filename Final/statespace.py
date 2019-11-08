a69cd5e1df12bea7c44319487c38c1c0c93056f3
raulbs7 <48548673+raulbs7@users.noreply.github.com>
2019-11-08 12:59:52 +0100

    Update statespace.py

--- Final/statespace.py
+++ Final/statespace.py
@@ -4,23 +4,17 @@ def sucessors(state):
 
 	moves = iter(state.generateMoves())
 
-	ret = [ Cube() for i in range(len(moves))]
+	ret = [ Cube() for i in range(len(state.generateMoves()))]
 
 	state.cube2Json("saved/"+"state.json")
 
-	for i in range(len(moves)):
+	for i in range(len(state.generateMoves())):
 		ret[i].json2cube("saved/"+"state.json")
 
-	for i in range(len(moves)):
+	for i in range(len(state.generateMoves())):
 
 		movement = next(moves)
 		ret[i].move(movement)
 		ret[i] = (movement,ret[i],1)
 
 	return ret
-
-print('Introduce a json filename')
-filename = input()
-c = Cube()
-c.json2cube(filename)
-print(sucessors(c))
