f79629f1799fbb5d51ccbe72cc65572aded61aae
Aharon1377 <48285149+Aharon1377@users.noreply.github.com>
2019-11-09 19:32:08 +0100

    Update main.py

--- Final/main.py
+++ Final/main.py
@@ -9,18 +9,23 @@ def writeFile(filename, text):
         outfile.write(text+"\n")
 
 print('--------- Introduce the type of algortihm: ---------')
-algortihm = str(input())
+algorithm = str(input())
 print('--------- Introduce the json filename of the Initial_state: ---------')
 filename_input = str(input())
 print('--------- Introduce the maximum depth of the nodes of the problem: ---------')
 depth = int(input())
-print('--------- Introduce the incremental constant of the depth (if it is necessary for IDS): ---------')
-inc_depth = int(input())
+if algorithm == "IDS":
+	print('--------- Introduce the incremental constant of the depth (if it is necessary for IDS): ---------')
+	inc_depth = int(input())
+else:
+	inc_depth = 0
 Prob = Problem(filename_input) #Clase Problem with InitialState and functions isGoal and sucessors
-sol = search(Prob, algortihm, depth, inc_depth)
-print('############### The SOLUTION of the cube with movements is the next one: #################')
-for i in range(len(sol)-1,0,-1):
-    print(sol[i-1].action)
-    writeFile(filename_output, sol[i-1].action)
-    writeFile(filename_output, str(sol[i-1].state.faces))
-
+sol = search(Prob, algorithm, depth, inc_depth)
+if sol is not None:
+	print('############### The SOLUTION of the cube with movements is the next one: #################')
+	for i in range(len(sol)-1,0,-1):
+	    print(sol[i-1].action)
+	    writeFile(filename_output, sol[i-1].action)
+	    writeFile(filename_output, str(sol[i-1].state.faces))
+else:
+	print('############### No solution was found #################')
